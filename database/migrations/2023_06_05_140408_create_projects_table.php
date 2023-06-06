cdb808a9ee5de3b398b731e9007ed4efce38114a
L-cir94 <luca.cirigliano94@hotmail.it>
2023-06-06 12:00:30 +0200

    seeding database

--- database/migrations/2023_06_05_140408_create_projects_table.php
+++ database/migrations/2023_06_05_140408_create_projects_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', '150')->unique();
+            $table->string('slug');
+            $table->string('cover_image')->nullable();
+            $table->text('content')->nullable();
             $table->timestamps();
         });
     }
--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +15,16 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+
+            $post = new Project();
+            $post->title = $faker->sentence(3);
+            $post->slug = Str::slug($post->title, '-');
+            $post->content = $faker->paragraphs(asText: true); 
+            $post->cover_image = $faker->imageUrl(category: 'Posts', format: 'jpg');
+            $post->save();
+        }
     }
 }
